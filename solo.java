2dc559b5e0beb59d905ffcac934a511ce936c3c1
sanidhyasinha17 <sanidhyasinha17@gmail.com>
2018-05-13 00:25:49 +0530

    Add files via upload

---
+++ solo.java
@@ -0,0 +1,19 @@
+public class Individual implements Comparable<Individual>{
+	String gene_;
+	double fitness_;
+	double [][] cMatrix;
+	Individual(){
+		fitness_=-1;
+	}
+	Individual(String _gene){
+		gene_ = _gene;
+		fitness_=-1;
+	}
+	public int compareTo(Individual otherIndividual) {
+		if(fitness_ > otherIndividual.fitness_)
+			return 1;
+		if(fitness_ < otherIndividual.fitness_)
+			return -1;
+		return 0;
+	}
+}
